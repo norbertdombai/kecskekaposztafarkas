e1a3cc472b43dd5107cc8ec6e77bef5dac2d35d5
Roli-t <78268292+Roli-t@users.noreply.github.com>
2021-04-14 19:20:25 +0200

    5,6 feladat kész, Tibai Roland

--- public_html/js.js
+++ public_html/js.js
@@ -1,7 +1,21 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+window.addEventListener("load",init);
 
+function init() {
+    $("#bal p")[0].addEventListener("mouseover",kiemel);
+    $("#bal p")[0].addEventListener("mouseout",kiemelLe);
+    
+}
 
+function kiemel() {
+    
+    $("#bal p")[0].classList.add("kiemel");
+}
+
+function $(nev) {
+    return document.querySelectorAll(nev);
+    
+}
+
+function kiemelLe() {
+    $("#bal p")[0].classList.remove("kiemel");
+}
