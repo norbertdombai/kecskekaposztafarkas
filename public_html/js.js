c6a5f18c64f22ffaec3ae23ba181154cd7c8f8a0
norbertdombai <55793506+norbertdombai@users.noreply.github.com>
2021-04-16 22:05:17 +0200

    7es felig megoldva

--- public_html/js.js
+++ public_html/js.js
@@ -1,13 +1,28 @@
 window.addEventListener("load",init);
 
 function init() {
-    $("#bal p")[0].addEventListener("mouseover",kiemel);
-    $("#bal p")[0].addEventListener("mouseout",kiemelLe);
+    $("#k")[0].addEventListener("mouseover",kiemel);
+    $("#k")[0].addEventListener("mouseout",kiemelLe);
+    $("#kp")[0].addEventListener("mouseover",kiemel1);
+    $("#kp")[0].addEventListener("mouseout",kiemelLe1);
+    $("#f")[0].addEventListener("mouseover",kiemel2);
+    $("#f")[0].addEventListener("mouseout",kiemelLe2);
+    $("#k")[0].addEventListener("click",eltuntet);
+    $("#kp")[0].addEventListener("click",eltuntet1);
+    $("#f")[0].addEventListener("click",eltuntet2);
     
 }
 
 function kiemel() {
-    $("#bal p")[0].classList.add("kiemel");
+    $("#k")[0].classList.add("kiemel");
+}
+
+function kiemel1() {
+    $("#kp")[0].classList.add("kiemel");
+}
+
+function kiemel2() {
+    $("#f")[0].classList.add("kiemel");
 }
 
 function $(nev) {
@@ -16,6 +31,30 @@ function $(nev) {
 }
 
 function kiemelLe() {
-    $("#bal p")[0].classList.remove("kiemel");
+    $("#k")[0].classList.remove("kiemel");
+    
 }
 
+function kiemelLe1() {
+    $("#kp")[0].classList.remove("kiemel");
+    }
+function kiemelLe2() {
+    $("#f")[0].classList.remove("kiemel");
+    }
+
+
+function eltuntet() {
+    $("#k")[0].classList.add("eltuntet");
+
+}
+
+
+function eltuntet1() {
+
+    $("#kp")[0].classList.add("eltuntet");
+}
+
+
+function eltuntet2() {
+    $("#f")[0].classList.add("eltuntet");
+}
