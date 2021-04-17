5223bf5b2f128494c8dae6a887cce903690658c6
norbertdombai <55793506+norbertdombai@users.noreply.github.com>
2021-04-17 13:19:25 +0200

    JS kodok javitott roviditett verzioja

--- public_html/js.js
+++ public_html/js.js
@@ -1,60 +1,29 @@
 window.addEventListener("load",init);
 
-function init() {
-    $("#k")[0].addEventListener("mouseover",kiemel);
-    $("#k")[0].addEventListener("mouseout",kiemelLe);
-    $("#kp")[0].addEventListener("mouseover",kiemel1);
-    $("#kp")[0].addEventListener("mouseout",kiemelLe1);
-    $("#f")[0].addEventListener("mouseover",kiemel2);
-    $("#f")[0].addEventListener("mouseout",kiemelLe2);
-    $("#k")[0].addEventListener("click",eltuntet);
-    $("#kp")[0].addEventListener("click",eltuntet1);
-    $("#f")[0].addEventListener("click",eltuntet2);
-    
-}
-
-function kiemel() {
-    $("#k")[0].classList.add("kiemel");
-}
-
-function kiemel1() {
-    $("#kp")[0].classList.add("kiemel");
-}
-
-function kiemel2() {
-    $("#f")[0].classList.add("kiemel");
-}
-
 function $(nev) {
     return document.querySelectorAll(nev);
     
 }
 
+function init() { 
+    for (var i = 0; i < 3; i++) {
+        $("#bal img")[i].addEventListener("mouseover",kiemel);
+        $("#bal img")[i].addEventListener("mouseout",kiemelLe);
+        $("#bal img")[i].addEventListener("click",eltuntet);
+    }}
+    
+
+function kiemel() {
+   this.classList.add("kiemel");
+}
+
 function kiemelLe() {
-    $("#k")[0].classList.remove("kiemel");
+   this.classList.remove("kiemel");
     
 }
 
-function kiemelLe1() {
-    $("#kp")[0].classList.remove("kiemel");
-    }
-function kiemelLe2() {
-    $("#f")[0].classList.remove("kiemel");
-    }
-
 
 function eltuntet() {
-    $("#k")[0].classList.add("eltuntet");
+    this.classList.add("eltuntet");
 
-}
-
-
-function eltuntet1() {
-
-    $("#kp")[0].classList.add("eltuntet");
-}
-
-
-function eltuntet2() {
-    $("#f")[0].classList.add("eltuntet");
 }
